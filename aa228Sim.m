3ca95ff6c4f5d793cecb5b27d495364bd1a8ab8a
John Alsterda <alstertda@stanford.edu>
2016-11-30 19:17:37 -0800

    Sim and drawWorld comments

--- aa228Sim.m
+++ aa228Sim.m
@@ -1,58 +1,63 @@
-% aa228Sim runs at the highest level of our simulation.  Call it to run our
-% game.
+% AA228SIM
+%
+% Run to our Forward Search MPD algorithm and simulation
+%
+% Authors: John, Molly, and Ramon
 
-close all;
+close all; % reset the game and all figures
 
-% Constants and variables
-simPeriod = .05;      % 10 msec, length of time between dynamic sims
-actPeriod = 2;        % 2 sec, take an action every 2 seconds
-playTime = 60;        % How many seconds to play a round?
-iterations = 60/.05;  % How many simPeriod rounds do we want to run?
-clear obstacles       % forg10ot last sim's obstacles
-action = [0 0];
+%% Constants and variables
+simPeriod = .05;        % 10 msec, length of time between dynamic sims
+actPeriod = 2;          % 2 sec, take an action every 2 seconds
+playTime = 60;          % How many seconds to play a round?
+iterations = 60/.05;    % How many simPeriod rounds do we want to run?
+clear obstacles;        % forget last sim's obstacles
+action = [0 0];         % initialize action to 'do nothing'
+% init rewards history vector, one entry for 2 sec time step
 rewards = zeros(1, playTime/actPeriod);
 
-% set up the road
+%% Initialize the simulation
+% open the 'AA228 Road' figure and draw the road (world holds handle)
+% open the 'AA228 Scoreboard' figure (score holds handle)
 [~, world, score] = drawWorld();
 % define a new set of random obstacles
 obstacles = updateObstacles();
 % define the agent
 agent = updateAgent();
 
-% move forward in time!
+%% move forward simPeriod seconds (actions only requested at actPeriods)
 for t = 1:iterations
-    
+    %% Draw the obstacles and agents at this period's locations
     % intialize rects graphics array to contain cars
     rectangles = gobjects(size(obstacles,1)+1);
-    
     % draw obstacles and agent
     [rectangles, handles] = drawWorld(obstacles, agent, rectangles, rewards, world, score);
-    
     % pause for some fraction of a second
     pause(simPeriod);    
+    
+    %% call for new action, propogate cars, calc reward
     
     % wait for action command, but only every actPeriod
     if mod(t*simPeriod,actPeriod) == 0
 %         action = getArrowAction(); % HMI: arrow keys
         action = getNumAction(); % HMI: NumPad
+%         action = runForwardSearch()?
     end
     
-    % remove the cars before proceeding to the next iteration
-    delete(rectangles);
-    
-    % push obstacles forward
+    % propogate obstacles forward
     obstacles = updateObstacles(obstacles); 
     agent = updateAgent(agent,action);
     
     % add the reward associated with this state-action
     if mod(t*simPeriod,actPeriod) == 0
-        
         rewards(t*simPeriod/actPeriod) = rewards(max([1 (t*simPeriod/actPeriod-1)])) + calcReward(action);
-
         figure(score);
         plot(rewards);
-        
     end
     
-    action = [0 0]; % reset action until next 2 second period.
+    %% cleanup before the end of this iteration
+    % remove the cars before proceeding to the next iteration
+    delete(rectangles);
+    % reset action until next 2 second period.
+    action = [0 0];
 end
--- drawWorld.m
+++ drawWorld.m
@@ -1,22 +1,20 @@
 function [rectangles, world, score, varargout] = drawWorld(obstacles, agent, rectangles, rewards, world, score, varargin)
 %DRAWWORLD Draw the environment, our car, and the other vehicles
 %   1. Draw the environment
-%       Road is 1000 meters long and 3*4 meters wide (+ 4 m grass x 2)
 %       Call once without obstacles to initialize figure
 %   2. Draw the obstacles
-%       Each obstacle is 6m long and 3m wide
 %       position in obstacles denotes C.O.M.
 %   3. Draw our car
 %   4. Draw scoreboard
-
+%
 % units: 1 meter = 1 pixel
 
-
-%   1.
 if nargin == 0
-    
+%%   1.    
+
     % create the figure with some properties
-    world = figure('Name', 'AA228 Road', 'Position',[0 200 1000*10 20*10]); %, 'OuterPosition',[0 100 1000*10 20*10]);
+    % Road is 1000 meters long and 3*4 meters wide (+ 4 m grass x 2) = 20
+    world = figure('Name', 'AA228 Road', 'Position',[0 200 1000*10 20*10]);
     hold on;
     % draw the grass
     grass  = rectangle('Position',[0 0 1000 20], 'FaceColor','g');
@@ -29,19 +27,18 @@ if nargin == 0
 
     rectangles = []; % just to have it ...
     
-%   2.
+
 elseif nargin > 0
-    
+%%   2.    
     figure(world);
-    
     % draw obstacles
     for i = 1:size(obstacles,1)
+        % Each obstacle is 6m long and 3m wide
         rearRightPos = [ obstacles(i,1)-3 , 4*obstacles(i,2)+.5];
         rectangles(i) = rectangle('Position',[ rearRightPos , 6 , 3], 'FaceColor', 'r');
     end
     
-%   3.
-    
+%%   3.
     % draw agent
     rearRightPos = [ agent(1)-3 , 4*agent(2)+.5];
     rectangles(end) = rectangle('Position',[ rearRightPos , 6 , 3], 'FaceColor', 'b');
@@ -51,19 +48,11 @@ end
 xlim([0 1000])
 ylim([0 20])
 
-%   4.
-
+%%   4.
 if nargin == 0
     % create the figure with some properties
-    score = figure('Name','AA 288 ScoreBoard','Position',[100 600 400 400]);
-
-% elseif nargin > 0
-%     
-%     length(rewards)
-%     figure(score);
-%     plot(rewards);
-%     hold on;
-%     
+    score = figure('Name','AA288 ScoreBoard','Position',[100 600 400 400]);
+  
 end
 
 end % EOF
